bdb501e01eca9e4bc488a011895a61ea6a32eb60
Elronia <zar.catherine@gmail.com>
2021-05-26 23:05:54 -0400

    solved Valid Anagram problem

--- Hashmap/script.js
+++ Hashmap/script.js
@@ -1,5 +1,29 @@
-
-
+var isAnagram = function(s, t) {
+    let hash = {};
+    
+    if (s.length !== t.length) return false;
+    
+    
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        if(hash[char]) {
+            hash[char] = hash[char] + 1; //hash[char] += 1 // hash[char] ++
+        }
+        else {
+            hash[char] = 1;
+        }
+    }
+    
+    for (let i = 0; i < t.length; i++) {
+        const char = t[i];
+        if(!hash[char]) {
+            return false; 
+        }
+        hash[char] = hash[char] - 1;
+    }
+    return true;
+      
+};
 
 
 // we create an empty hashmap
