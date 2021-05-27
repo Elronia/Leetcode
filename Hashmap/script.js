f91500c9546baaa6a50243dbe2117252517a44ee
Elronia <zar.catherine@gmail.com>
2021-05-26 23:05:01 -0400

    solving Valid Anagram problem using hashmap

---
+++ Hashmap/script.js
@@ -0,0 +1,9 @@
+
+
+
+
+// we create an empty hashmap
+// loop through the 1st string
+// if letter isn't in a hashmap we'll create a key, 
+// if it is in a hashmap we'll increment value and we'll go to the next symbol in a string
+// loop over the 2nd string and compare with symbols in a hashmap
