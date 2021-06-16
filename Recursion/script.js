34e9d21255a011f569466dbf6c9349e458889be4
Elronia <zar.catherine@gmail.com>
2021-06-15 23:34:07 -0400

    solved Fibonacci Number recursively

--- Recursion/script.js
+++ Recursion/script.js
@@ -1,4 +1,10 @@
+// var fib = function(n) {
+//     if(n <= 1) return n;
+//     return fib(n - 1) + fib(n - 2);
+// };
+
 var fib = function(n) {
-    if(n <= 1) return n;
+    if(n < 2) return n;
     return fib(n - 1) + fib(n - 2);
 };
+//0, 1, 1, 2, 3, 5, 8, 13, 21, 34
